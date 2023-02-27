7467fa12a5809bedcf6a3dba5022f172b689a4bc
evridiky <120931048+evridiky@users.noreply.github.com>
2023-02-27 19:52:09 +0200

    Update BooksAPI.java

--- BooksAPI/src/main/java/booksapi/BooksAPI.java
+++ BooksAPI/src/main/java/booksapi/BooksAPI.java
@@ -7,14 +7,17 @@ public class BooksAPI {
 	//αρχικοποίηση του BooksAPIService 
 	
 public static BooksAPIService getBooksDBService() {
-		return new BooksAPIService("https://www.googleapis.com/books/v1/volumes?", "AIzaSyAMm8wdV_RFt2Dq6NE-FrdemuseQxbmkKU");	
+		//return new BooksAPIService("https://www.googleapis.com/books/v1/volumes?", "AIzaSyAMm8wdV_RFt2Dq6NE-FrdemuseQxbmkKU");	
+		return new BooksAPIService("https://www.googleapis.com/books/v1/volumes?");	
 	}
 
 public static BooksAPIService getBookshelfDBService() {
 	// TODO Auto-generated method stub
-	 return new BooksAPIService("https://www.googleapis.com/books/v1/users/", "AIzaSyAMm8wdV_RFt2Dq6NE-FrdemuseQxbmkKU");
+	// return new BooksAPIService("https://www.googleapis.com/books/v1/users/", "AIzaSyAMm8wdV_RFt2Dq6NE-FrdemuseQxbmkKU");
+	 return new BooksAPIService("https://www.googleapis.com/books/v1/users/");
  }
 
 
 }
+
 
